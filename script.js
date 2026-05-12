1cf7ccdae24f178ec0fdc182087e71b82c3d4cdf
YashG2606 <gawaiyash26@gmail.com>
2026-05-12 18:32:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ function yes(){
 
 document.getElementById("quiz").innerHTML = `
 
+<div class="fade">
+
 <h2>Quiz Time ❤️</h2>
 
 <p>Do you remember when we first met? 🥺</p>
-<input type="text" placeholder="Your answer"><br><br>
+<input type="text"><br><br>
 
-<p>What is the best thing you like about me? ❤️</p>
-<input type="text" placeholder="Type here"><br><br>
+<p>What do you like most about me? ❤️</p>
+<input type="text"><br><br>
 
 <p>What do you dislike about me? 😔</p>
-<input type="text" placeholder="Be honest"><br><br>
+<input type="text"><br><br>
 
 <p>Why aren't you talking with me...? 🥹</p>
-<textarea placeholder="Tell me..."></textarea><br><br>
+<textarea></textarea><br><br>
 
 <h3>Ab to maan ja meri jaan ❤️</h3>
 
-<img src="https://i.ibb.co/S7shrBSG/Whats-App-Image-2026-05-03-at-22-27-27.jpg" width="200">
+<img src="YOUR_IMAGE_LINK" width="200">
+
+</div>
 
 `;
 
-}
-
-function no(){
-alert("Wrong answer 😭");
 }
