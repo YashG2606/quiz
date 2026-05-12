856edb4049a62cb48d7010d1fbd1b19473ff0438
YashG2606 <gawaiyash26@gmail.com>
2026-05-12 17:57:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function yes(){
+  document.getElementById("quiz").innerHTML = `
+  <h2>Quiz Time ❤️</h2>
+  <p>Our first memory?</p>
+  <img src="https://i.imgur.com/4AiXzf8.jpeg" width="200">
+  `;
+}
+
+function no(){
+  alert("Wrong answer 😭");
+}
