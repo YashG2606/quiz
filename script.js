58bded31c54d502c36d2be1321833499cc7e0b45
YashG2606 <gawaiyash26@gmail.com>
2026-05-12 18:10:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 function yes(){
-  document.getElementById("quiz").innerHTML = `
-  <h2>Quiz Time ❤️</h2>
-  <p>Our first memory?</p>
-  <img src="https://i.imgur.com/4AiXzf8.jpeg" width="200">
-  `;
+
+document.getElementById("quiz").innerHTML = `
+
+<h2>Quiz Time ❤️</h2>
+
+<p>Do you remember when we first met? 🥺</p>
+<input type="text" placeholder="Your answer"><br><br>
+
+<p>What is the best thing you like about me? ❤️</p>
+<input type="text" placeholder="Type here"><br><br>
+
+<p>What do you dislike about me? 😔</p>
+<input type="text" placeholder="Be honest"><br><br>
+
+<p>Why aren't you talking with me...? 🥹</p>
+<textarea placeholder="Tell me..."></textarea><br><br>
+
+<h3>Ab to maan ja meri jaan ❤️</h3>
+
+<img src="https://ibb.co/zHWx5T9j" width="200">
+
+`;
+
 }
 
 function no(){
-  alert("Wrong answer 😭");
+alert("Wrong answer 😭");
 }
